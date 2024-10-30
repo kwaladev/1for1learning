03efd5d9d0b41145b76ee926e1e943344dd3d920
aminefeidi <amine.feidi@gmail.com>
2024-10-30 22:19:15 +0100

    ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
